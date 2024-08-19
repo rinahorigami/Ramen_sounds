ed442dad276e08a4a7bf38f8c9b876d5dde0e435
rinahorigami <h.rina6604@gmail.com>
2024-08-20 01:57:30 +0900

    フラッシュメッセージ #40

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,15 @@ class SessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_to videos_path
+      redirect_to videos_path, flash: { success: "ログインしました。" }
     else
-      render :new
+      flash.now[:danger] = "ログインに失敗しました。"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, flash: { success: "ログアウトしました。" }
   end
 end
