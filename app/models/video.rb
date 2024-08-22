3a3db6467e8db084de9119c73f0f3388760fb771
rinahorigami <h.rina6604@gmail.com>
2024-08-22 17:26:03 +0900

    Amazon S3とCarrierWaveを連携 #44

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,12 @@ class Video < ApplicationRecord
   belongs_to :ramen_shop
 
   validates :file, presence: true
+
+  before_destroy :remove_file_from_s3
+
+  private
+
+  def remove_file_from_s3
+    file.remove!
+  end
 end
