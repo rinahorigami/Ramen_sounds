9831f8d5081cbefee23f1a89721c8c464aed1f7e
rinahorigami <h.rina6604@gmail.com>
2024-08-22 12:19:50 +0000

    マイグレーションファイル修正

--- db/migrate/20240815075352_rename_title_and_description_in_videos.rb
+++ db/migrate/20240815075352_rename_title_and_description_in_videos.rb
@@ -1,25 +1,17 @@
 class RenameTitleAndDescriptionInVideos < ActiveRecord::Migration[7.0]
-  def up
+  def change
     if column_exists?(:videos, :title)
       rename_column :videos, :title, :menu_name
     end
-
+  
     if column_exists?(:videos, :description)
       rename_column :videos, :description, :price
     end
     
     Video.where(price: '').update_all(price: nil) if column_exists?(:videos, :price)
-    change_column :videos, :price, :integer if column_exists?(:videos, :price)
-  end
-
-  def down
-    if column_exists?(:videos, :menu_name)
-      rename_column :videos, :menu_name, :title
-    end
 
     if column_exists?(:videos, :price)
-      change_column :videos, :price, :string
-      rename_column :videos, :price, :description
+      change_column :videos, :price, 'integer USING CAST(price AS integer)'
     end
   end
 end
