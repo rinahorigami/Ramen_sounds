36f0d8fa56e548b3ed580c4b4ff01b39023446d1
rinahorigami <h.rina6604@gmail.com>
2024-09-05 21:33:56 +0900

    投稿機能のバグ修正(店舗選択) #74

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,7 +14,7 @@ class VideosController < ApplicationController
     ramen_shop_id = params[:ramen_shop_id]
     
     if ramen_shop_id.present?
-      google_places_service = GooglePlacesService.new
+      google_places_service = GooglePlacesService.new(ENV['GOOGLE_PLACES_API_KEY'])
       ramen_shop_data = google_places_service.get_ramen_shop_data(ramen_shop_id)
       
       @ramen_shop = RamenShop.find_or_initialize_by(place_id: ramen_shop_id)
@@ -49,6 +49,31 @@ class VideosController < ApplicationController
   def edit ;end
 
   def update
+    ramen_shop_id = params[:ramen_shop_id]
+  
+    if ramen_shop_id.present?
+      google_places_service = GooglePlacesService.new(ENV['GOOGLE_PLACES_API_KEY'])
+      ramen_shop_data = google_places_service.get_ramen_shop_data(ramen_shop_id)
+  
+      @ramen_shop = RamenShop.find_or_initialize_by(place_id: ramen_shop_id)
+      @ramen_shop.name = ramen_shop_data['name']
+      @ramen_shop.address = ramen_shop_data['formatted_address']
+      @ramen_shop.phone_number = ramen_shop_data['formatted_phone_number']
+      @ramen_shop.latitude = ramen_shop_data['geometry']['location']['lat']
+      @ramen_shop.longitude = ramen_shop_data['geometry']['location']['lng']
+  
+      if ramen_shop_data['opening_hours'].present?
+        @ramen_shop.opening_hours = ramen_shop_data['opening_hours']['weekday_text'].join("\n")
+      end
+  
+      @ramen_shop.save
+      @video.ramen_shop = @ramen_shop
+      @video.place_id = ramen_shop_id
+    else
+      @video.ramen_shop = nil
+      @video.place_id = nil
+    end
+  
     if @video.update(video_params)
       flash[:notice] = "動画を編集しました。"
       redirect_to videos_path
