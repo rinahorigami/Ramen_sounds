f309ee62a213a3c0f45366d492145580be745fe9
rinahorigami <h.rina6604@gmail.com>
2024-09-18 14:24:20 +0900

    パスワードリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,15 +73,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'ramen-sounds.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
-    port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: ENV['SMTP_AUTHENTICATION'],
-    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO']
-  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
