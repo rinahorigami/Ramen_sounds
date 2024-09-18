0e08653a093175e5585d05fc0d5bc75a9982165b
rinahorigami <h.rina6604@gmail.com>
2024-09-18 12:17:36 +0900

    パスワードリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = Settings.mailer.smtp_settings.to_h
   config.action_mailer.perform_deliveries = true
 
@@ -72,7 +71,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'noreply@ramensounds.com' }
+  config.action_mailer.default_url_options = { host: 'ramen-sounds.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: ENV['SMTP_AUTHENTICATION'],
+    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
