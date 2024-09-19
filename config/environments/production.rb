6c74ba65e10bf4a6b44b1492ddb9b2d511f98e0d
rinahorigami <h.rina6604@gmail.com>
2024-09-19 16:10:12 +0900

    パスワードリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.smtp_settings = Settings.mailer.smtp_settings.to_h
   config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -73,6 +72,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'ramen-sounds.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.zoho.jp",
+    port: 587,
+    domain: "ramen-sounds.com",
+    user_name: ENV['SMTP_USER_NAME'],  # 環境変数から取得
+    password: ENV['SMTP_PASSWORD'],    # 環境変数から取得
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
