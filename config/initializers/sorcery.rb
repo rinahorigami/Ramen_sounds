f6ca083557ad8cd22c160fef0934c3377e961919
rinahorigami <h.rina6604@gmail.com>
2024-10-11 19:39:03 +0900

    Googleログイン #126

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,7 +4,7 @@
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
 # :magic_login, :external
-Rails.application.config.sorcery.submodules = [:reset_password, :remember_me]
+Rails.application.config.sorcery.submodules = [:reset_password, :remember_me, :external]
 
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
@@ -80,7 +80,13 @@ Rails.application.config.sorcery.configure do |config|
   # i.e. [:twitter, :facebook, :github, :linkedin, :xing, :google, :liveid, :salesforce, :slack, :line].
   # Default: `[]`
   #
-  # config.external_providers =
+  config.external_providers = %i[google]
+
+  # GoogleのOAuth設定
+  config.google.key = ENV["GOOGLE_CLIENT_ID"]
+  config.google.secret = ENV["GOOGLE_CLIENT_SECRET"]
+  config.google.callback_url = "http://localhost:3000/google_login_api/callback"
+  config.google.user_info_mapping = {email: "email", name: "name" }
 
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
@@ -319,7 +325,7 @@ Rails.application.config.sorcery.configure do |config|
     # How long in seconds the session length will be
     # Default: `60 * 60 * 24 * 7`
     #
-    user.remember_me_for = 1.month
+    user.remember_me_for = 2.weeks
 
     # When true, sorcery will persist a single remember me token for all
     # logins/logouts (to support remembering on multiple browsers simultaneously).
@@ -543,7 +549,7 @@ Rails.application.config.sorcery.configure do |config|
     # Class which holds the various external provider data for this user.
     # Default: `nil`
     #
-    # user.authentications_class =
+    user.authentications_class = Authentication
 
     # User's identifier in the `authentications` class.
     # Default: `:user_id`
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_07_070504) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_10_132650) do
+  create_table "authentications", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid", unique: true
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.text "content", null: false
     t.bigint "user_id", null: false
@@ -91,6 +101,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_07_070504) do
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
+  add_foreign_key "authentications", "users"
   add_foreign_key "comments", "users"
   add_foreign_key "comments", "videos"
   add_foreign_key "likes", "users"
