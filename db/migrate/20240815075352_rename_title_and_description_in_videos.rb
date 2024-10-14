a42a302237703362fb242e16d1d0136ba3ff5c2f
rinahorigami <h.rina6604@gmail.com>
2024-10-14 23:02:34 +0900

    ciのrspec実行時のデータベースエラーの修正

--- db/migrate/20240815075352_rename_title_and_description_in_videos.rb
+++ db/migrate/20240815075352_rename_title_and_description_in_videos.rb
@@ -1,17 +1,20 @@
 class RenameTitleAndDescriptionInVideos < ActiveRecord::Migration[7.0]
   def change
+    # 既存のカラムをリネーム（存在を確認してからリネーム）
     if column_exists?(:videos, :title)
       rename_column :videos, :title, :menu_name
     end
-  
+
     if column_exists?(:videos, :description)
       rename_column :videos, :description, :price
     end
-    
+
+    # priceカラムの空文字列をnilに変換
     Video.where(price: '').update_all(price: nil) if column_exists?(:videos, :price)
 
+    # priceカラムの型を変更
     if column_exists?(:videos, :price)
-      change_column :videos, :price, 'integer USING CAST(price AS integer)'
+      change_column :videos, :price, :integer
     end
   end
 end
