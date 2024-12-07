04127d88116c5f358a052f7e4067965da45e5cc3
rinahorigami <h.rina6604@gmail.com>
2024-12-07 18:16:11 +0900

    いいね機能のコードリファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,19 +3,11 @@ class LikesController < ApplicationController
 
   def create
     @like = @video.likes.create(user: current_user)
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to @video }
-    end
   end
 
   def destroy
     @like = @video.likes.find_by(user: current_user)
     @like.destroy if @like
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to @video }
-    end
   end
 
   private
